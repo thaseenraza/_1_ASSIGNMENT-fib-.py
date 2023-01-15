2215769facd9d9b0bc206769cef5216c3e13ff8a
thaseenraza <122023796+thaseenraza@users.noreply.github.com>
2023-01-15 15:29:16 +0530

    ASSIGNMENT_1_QUESTION-2

---
+++ _2_ASSIGNMENT(word).py
@@ -0,0 +1,8 @@
+# Write a Python program that accepts a word from the user and reverse it.
+# Sample Test Case
+# Input : Edyoda
+# output: adoydE
+String = str(input("Enter the Input name : "))
+print(String)
+String2 = String[-1 ::-1]
+print(String2)
